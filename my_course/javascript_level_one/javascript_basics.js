192fc6f908532eb4cef345280bcefc94c774b26f
Timothy <te25son@gmail.com>
2018-11-09 07:50:39 +0100

    add new folder - JavaScript level one

---
+++ my_course/javascript_level_one/javascript_basics.js
@@ -0,0 +1,67 @@
+// this is a comment
+
+alert("Hello World!")
+
+// Basic Data Types
+// Intergers, Floats and Negatives are all treated as numbers in Javascript
+10
+20.2
+-13.4
+
+// Strings - characters wrapped in quotes
+"Hello"
+"10"
+"888.88"
+
+// Booleans - use lowercase
+true
+false
+
+// Undefined and Null
+undefined
+null
+
+// Calculating numbers
+// Addition
+2+2
+// Subtraction
+2-2
+// Multiplication
+2*2
+// Division
+2/2
+// Power
+2 ** 4  // 16
+// Modulo
+15 % 14 //1 - returns the remainder of a division
+
+// Strings
+// Concatinate Strings
+"Hello" + " " + "there!"
+
+// Indexing
+"Hello"[0] // H
+"Hello"["Hello".length - 1] // o
+
+"Look at the dog".length
+
+// Variables
+// var varName = value;
+var bankAccount = 100;
+
+var deposit = 50;
+
+var total = bankAccount + deposit;
+
+var greeting = "Welcome Back: ";
+
+var name = "Jose";
+
+// Others
+alert(greeting+name)
+
+console.log("Something")  // adds to the console
+
+prompt("Something")  // calls a prompt that you can type to
+
+var age = promp("What's your age?")
