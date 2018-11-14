a09f7ba262ae258cde54ddc6a132b4cd264ba90a
Timothy <te25son@gmail.com>
2018-11-14 07:42:06 +0100

    add while loop lesson examples

---
+++ my_course/javascript_level_one/while.js
@@ -0,0 +1,54 @@
+// while loop will continuously run code as long the condition remains true
+
+// syntax
+while (true) {
+  // execute some conde
+  // while this is true
+}
+
+ var x = 0;
+
+ while (x < 5) {
+   console.log('x is: ' + x);
+   console.log('x is still less than 5, adding 1 to x...');
+   x = x ** 2
+ }
+
+ var x = 0;
+
+ while (x < 5) {
+   console.log('x is: ' + x);
+   if (x === 3) {
+     console.log('x is EQUAL to 3');
+     break;
+   }
+   console.log('x is still less than 5, adding 1 to x...');
+   x = x + 1
+ }
+
+ // age example
+ var age = prompt('What\'s your age?')
+ var guess = 0
+
+ while (age >= guess) {
+
+     console.log("Are you " + guess + "?");
+
+   if (guess == age) {
+     console.log("I got it! You're " + guess);
+     break;
+   }
+
+   console.log("No? Let me guess again...");
+   guess = guess + 1
+ }
+
+ // while loop 1 - 10
+var x = 0
+while (x <= 10) {
+  if (x % 2 == 0) {
+    console.log(x);
+    break;
+  }
+  x = x + 1
+}
