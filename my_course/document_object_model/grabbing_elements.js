36f254a68e4bbcec31164561c12fd1d46c2e80c2
Timothy <te25son@gmail.com>
2018-11-24 12:29:44 +0100

    add important methods and attributes for grabbing elements

---
+++ my_course/document_object_model/grabbing_elements.js
@@ -0,0 +1,16 @@
+// DOM (document object model)
+
+// Grabbing elements from the document model
+
+// important document attributes:
+document.URL  // grabs the document URL
+document.body  // grabs the documents HTML body
+document.links  // grabs the document HTML links
+document.head // grabs the document HTML head
+
+// methods for grabing specific elemtents from the DOM
+document.getElementById()  // returns an element with whatever ID you pass in
+document.getElementsByClassName()  // returns a list of all items belonging to the given class
+document.getElementsByTagName()  // returns a list of all elements with a certain tag
+document.querySelector()  // returns the first object containing the css style selector
+document.querySelectorAll()  // returns all objects containing the css style selector
