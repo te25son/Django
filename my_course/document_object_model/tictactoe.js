771e7c8f206f355bf39e995d7e7b83dee3e61ce0
Timothy <te25son@gmail.com>
2018-11-29 08:15:34 +0100

    add modified tic tac toe project (refractored, but still missing some desired abilities)

--- my_course/document_object_model/tictactoe.js
+++ my_course/document_object_model/tictactoe.js
@@ -1,46 +1,42 @@
 var start = document.querySelector('#start')
 var game = document.querySelector('#game')
 
-var cellOne = document.querySelector('#one')
-var cellTwo = document.querySelector('#two')
-var cellThree = document.querySelector('#three')
-var cellFour = document.querySelector('#four')
-var cellFive = document.querySelector('#five')
-var cellSix = document.querySelector('#six')
-var cellSeven = document.querySelector('#seven')
-var cellEight = document.querySelector('#eight')
-var cellNine = document.querySelector('#nine')
-
-var clickCount = 0
 
 start.addEventListener('click', gameStart)
 
-cellOne.addEventListener('click', play)
-cellTwo.addEventListener('click', play)
-cellThree.addEventListener('click', play)
-cellFour.addEventListener('click', play)
-cellFive.addEventListener('click', play)
-cellSix.addEventListener('click', play)
-cellSeven.addEventListener('click', play)
-cellEight.addEventListener('click', play)
-cellNine.addEventListener('click', play)
+// get all cells with DOM
+var cells = document.querySelectorAll('td')
 
+// give function play() to all cells with for loop
+for (var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', play)
+}
+
+// add game start function
 function gameStart() {
   alert("The game has started. X goes first.");
   game.style.visibility = 'visible';
 }
 
+// add array to keep track of moves made
+var clickCount = 0
+
+// add function to play
 function play() {
-  if (clickCount % 2 == 0) {
-    this.textContent = 'X';
-    this.style.background = '#5f5f5f';
-    this.style.color = "white";
-    clickCount++
+  if (this.textContent === '') {
+    if (clickCount % 2 == 0) {
+      this.textContent = 'X';
+      this.style.background = '#5f5f5f';
+      this.style.color = "white";
+      clickCount++
+    } else {
+      this.textContent = 'O';
+      this.style.background = '#5f5f5f';
+      this.style.color = "white";
+      clickCount++
+    }
   } else {
-    this.textContent = 'O';
-    this.style.background = '#5f5f5f';
-    this.style.color = "white";
-    clickCount++
+    alert('Square already taken. Choose another.')
   }
 }
 
