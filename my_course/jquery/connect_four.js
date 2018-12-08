91571a6d0d5cebd816a3c3d6a5ec102ce87e3579
Timothy <te25son@gmail.com>
2018-12-08 13:38:14 +0100

    fix the buggy stuff i left from last night

--- my_course/jquery/connect_four.js
+++ my_course/jquery/connect_four.js
@@ -1,6 +1,7 @@
 // get players and player colors
 var bluePlayer = prompt('Player 1 is Blue. Enter player 1\'s name.');
 var bluePlayerColor = '#206fcf';
+
 var redPlayer = prompt('Player 2 is Red. Enter player 2\'s name.');
 var redPlayerColor = '#cf204e';
 
@@ -9,21 +10,21 @@ var game_on = true;
 var table = $('table tr');
 
 // create function to change button color in table
-function dropColor(rowIndex, colIndex, color) {
-  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color', color);
+function changeColor(rowIndex, colIndex, color) {
+  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
 }
 
 // create function to get existing color of button in table
-function reportColor(rowIndex, colIndex) {
+function returnColor(rowIndex, colIndex) {
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
 }
 
 // create function to drop the color to the bottom of a column
-function goToBottom(colIndex) {
-  var report = reportColor(5, colIndex);
-  for (var row = 5; row > -1; i--) {
-    report = reportColor(row, colIndex);
-    if (report != '#cf204e' && report != '#206fcf') {
+function checkBottom(colIndex) {
+  var colorReport = returnColor(5, colIndex);
+  for (var row = 5; row > -1; row--) {
+    colorReport = returnColor(row, colIndex);
+    if (colorReport === 'rgb(218, 218, 218)') {
       return row
     }
   }
@@ -31,8 +32,8 @@ function goToBottom(colIndex) {
 
 // #ffffff
 // check for color matches
-function colorMatchCheck (one, two, three, four) {
-  return (one === two && one === three && one === four && one !== '#ffffff' && one !== undefined)
+function colorMatchCheck(one, two, three, four) {
+  return (one === two && one === three && one === four && one !== 'rgb(218, 218, 218)' && one !== undefined);
 }
 
 // check for wins
@@ -40,10 +41,10 @@ function colorMatchCheck (one, two, three, four) {
 function horizontalWinCheck() {
   for (var row = 0; row < 6; row++) {
     for (var col = 0; col < 4; col++) {
-      if (colorMatchCheck(reportColor(row, col), reportColor(row, col+1), reportColor(row, col+2), reportColor(row, col+3))) {
+      if (colorMatchCheck(returnColor(row, col), returnColor(row, col+1) ,returnColor(row, col+2), returnColor(row, col+3))) {
         console.log('Horizontal Win');
-        return true
-      } else {
+        return true;
+      }else {
         continue;
       }
     }
@@ -54,10 +55,10 @@ function horizontalWinCheck() {
 function verticalWinCheck() {
   for (var col = 0; col < 7; col++) {
     for (var row = 0; row < 3; row++) {
-      if (colorMatchCheck(reportColor(row, col), reportColor(row+1, col), reportColor(row+2, col), reportColor(row+3, col))) {
+      if (colorMatchCheck(returnColor(row, col), returnColor(row+1, col) ,returnColor(row+2, col), returnColor(row+3, col))) {
         console.log('Vertical Win');
-        return true
-      } else {
+        return true;
+      }else {
         continue;
       }
     }
@@ -68,37 +69,65 @@ function verticalWinCheck() {
 function diagonalWinCheck() {
   for (var col = 0; col < 5; col++) {
     for (var row = 0; row < 7; row++) {
-      if (colorMatchCheck(reportColor(row, col), reportColor(row+1, col+1), reportColor(row+2, col+2), reportColor(row+3, col+3))) {
+      if (colorMatchCheck(returnColor(row, col), returnColor(row+1, col+1) ,returnColor(row+2, col+2), returnColor(row+3, col+3))) {
         console.log('Diagonal Win');
-        return true
-      } else if (colorMatchCheck(reportColor(row, col), reportColor(row-1, col+1), reportColor(row-2, col+2), reportColor(row-3, col+3))) {
+        return true;
+      }else if (colorMatchCheck(returnColor(row, col), returnColor(row-1, col+1) ,returnColor(row-2, col+2), returnColor(row-3, col+3))) {
         console.log('Diagonal Win');
-        return true
+        return true;
+      }else {
+        continue;
       }
     }
   }
 }
 
-// game logic
-// start with blue player
+// Game End
+function gameEnd(winningPlayer) {
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 7; row++) {
+      $('#message').fadeOut('fast');
+      $('h1').text(winningPlayer+" has won!");
+      $('p').text('Refresh your browser to play again.');
+    }
+  }
+}
+
+// Start with Player One
 var currentPlayer = 1;
 var currentName = bluePlayer;
 var currentColor = bluePlayerColor;
 
-// change the message
-$('#message').text(bluePlayer + " choose a row to drop your tile.")
+// Start with Player One
+$('#message').text(bluePlayer + " choose a column to drop your color.");
 
-$('table button').on('click', function() {
-  // get row that has been clicked
-  var col = $('this').closest('td').index();
-  // get the bottom of that row
-  var bottomAvail = goToBottom(col);
-  //drop the chip into the bottom available row
-  dropColor(bottomAvail, col, currentColor);
-  // check if someone has won
+$('.board button').on('click',function() {
+
+  // Recognize what column was chosen
+  var col = $(this).closest("td").index();
+
+  // Get back bottom available row to change
+  var bottomAvail = checkBottom(col);
+
+  // Drop the chip in that column at the bottomAvail Row
+  changeColor(bottomAvail, col, currentColor);
+
+  // Check for a win or a tie.
   if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
-    $('h1').text(currentName + "has won!");
-    $('p').eq(0).text('Refresh the browser to play again.');
-    $('#message').fadeOut('fast');
+    gameEnd(currentName);
+  }
+
+  // If no win or tie, continue to next player
+  currentPlayer = currentPlayer * -1 ;
+
+  // Re-Check who the current Player is.
+  if (currentPlayer === 1) {
+    currentName = bluePlayer;
+    $('#message').text(currentName +" it is your turn, please pick a column to drop your blue chip.");
+    currentColor = bluePlayerColor;
+  }else {
+    currentName = redPlayer
+    $('#message').text(currentName + " it is your turn, please pick a column to drop your red chip.");
+    currentColor = redPlayerColor;
   }
 })
